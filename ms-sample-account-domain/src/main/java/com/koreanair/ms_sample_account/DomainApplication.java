9cc09e66e5150da1cdbd4cff20f358d968d2c89d
helpkang <helpkang@gmail.com>
2019-08-16 16:05:03 +0900

    domain spring boot main annotation remove

--- ms-sample-account-domain/src/main/java/com/koreanair/ms_sample_account/DomainApplication.java
+++ ms-sample-account-domain/src/main/java/com/koreanair/ms_sample_account/DomainApplication.java
@@ -1,9 +1,7 @@
 package com.koreanair.ms_sample_account;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class DomainApplication {
 
 	public static void main(String[] args) {
