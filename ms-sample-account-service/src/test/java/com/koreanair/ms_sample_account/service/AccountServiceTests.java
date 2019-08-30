f63a7d72ab01f1de7dcb4b26dad6972e2bb20179
helpkang <helpkang@gmail.com>
2019-08-30 11:45:39 +0900

    transfer query paging

--- ms-sample-account-service/src/test/java/com/koreanair/ms_sample_account/service/AccountServiceTests.java
+++ ms-sample-account-service/src/test/java/com/koreanair/ms_sample_account/service/AccountServiceTests.java
@@ -3,33 +3,41 @@ package com.koreanair.ms_sample_account.service;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
-
+import com.koreanair.ms_sample_account.domain.account.Account;
+import com.koreanair.ms_sample_account.domain.account.Transfer;
+import com.koreanair.ms_sample_account.repository.AccountRepository;
 import com.koreanair.ms_sample_account.service.vo.AccountVO;
 import com.koreanair.ms_sample_account.service.vo.CreateAccountVO;
 import com.koreanair.ms_sample_account.service.vo.TransferAccountVO;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import lombok.extern.slf4j.Slf4j;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 public class AccountServiceTests {
 
 	@Autowired
 	AccountService service;
 
+	@Autowired
+	AccountRepository accountRepository;
+
 	@Test
-	@Transactional
 	public void accountCreate() {
 		service.createAccount(createCreateAccount("a", 100));
 		assertEquals(100, getBalance("a"));
 	}
 
 	@Test
-	@Transactional
 	public void transfer() {
 		service.createAccount(createCreateAccount("a", 100));
 		service.createAccount(createCreateAccount("b", 200));
@@ -37,6 +45,30 @@ public class AccountServiceTests {
 		assertEquals(50, getBalance("a"));
 		assertEquals(250, getBalance("b"));
 	}
+	
+
+	@Test
+	public void transferPaging() {
+		String from = "from_";
+		String to = "to_";
+		service.createAccount(createCreateAccount(from, 100));
+		service.createAccount(createCreateAccount(to, 200));
+		service.transfer(TransferAccountVO.builder().from(from).to(to).amount(1).build());
+		service.transfer(TransferAccountVO.builder().from(from).to(to).amount(2).build());
+		service.transfer(TransferAccountVO.builder().from(from).to(to).amount(3).build());
+		service.transfer(TransferAccountVO.builder().from(from).to(to).amount(4).build());
+		Account fromAccount = accountRepository.findById(from).get();
+
+		PageRequest pp = PageRequest.of(0, 2);
+		Page<Transfer> page = accountRepository.findBy(fromAccount, pp);
+		log.debug("page: {}", page);
+		
+		page.get().forEach(
+			(transaction)->{log.debug("transaction:{}", transaction);}
+		);
+		
+	}
+	
 	
 	
 	private int getBalance(String name){
