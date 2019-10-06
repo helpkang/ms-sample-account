fc61244a5ecaa74680bd8ce1adda1a74a8b34428
이병덕 <bbangduk@gmail.com>
2019-10-07 07:27:07 +0900

    eretail 관련수정

---
+++ common-adapter/src/main/java/com/koreanair/common_adapter/eretail/ERetailBaseAdapter.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2011-2019 KoreanAir
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.koreanair.common_adapter.eretail;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ERetailBaseAdapter {
+	protected String jsessionid = "";
+	protected Object returnObject = null;
+
+	public String getJsessionid() {
+		return jsessionid;
+	}
+
+	public void setJsessionid(String jsessionid) {
+		this.jsessionid = jsessionid;
+	}
+
+	public Object getReturnObject() {
+		return returnObject;
+	}
+
+	public void setReturnObject(Object returnObject) {
+		this.returnObject = returnObject;
+	}
+
+}
---
+++ common-adapter/src/main/java/com/koreanair/common_adapter/eretail/override/OverrideAdapter.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2011-2019 KoreanAir
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.koreanair.common_adapter.eretail.override;
+
+import javax.xml.soap.SOAPException;
+
+import com.koreanair.common_adapter.eretail.ERetailBaseAdapter;
+import com.koreanair.common_adapter.eretail.connector.ERetailConnector;
+import com.koreanair.common_adapter.eretail.connector.ERetailSoapConnectorImpl;
+import com.koreanair.common_adapter.utils.JAXBFactory;
+import com.koreanair.common_adapter.utils.SchemaLocation;
+import com.koreanair.external.eretail.vo.common.overrideinput.OverrideInput;
+import com.koreanair.external.eretail.vo.common.overrideinput.OverrideInput.EMBEDDEDTRANSACTION;
+import com.koreanair.external.eretail.vo.common.overrideoutput.OverrideOutput;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * <pre>
+ * Created by bdlee on 2019. 10. 3.
+ * </pre>
+ *
+ * @author bdlee
+ *
+ * @version 1.0.0
+ * @since 2019. 10. 3.
+ */
+@Slf4j
+public class OverrideAdapter extends ERetailBaseAdapter {
+	private ERetailConnector retailCon = new ERetailSoapConnectorImpl();	// SOAPMesage 기반 connector
+
+	public OverrideInput getEmbeddedOverrideInput(Object embeddedObject) {
+		OverrideInput overrideInupt = new OverrideInput();
+		overrideInupt.setTRANSACTIONID("Override");
+		overrideInupt.setSITE("CBFICBFI");
+		overrideInupt.setLANGUAGE("GB");
+		overrideInupt.setSOSITEOFFICEID("SELKE08DM");
+		overrideInupt.setSOSITEMINAVAILDATESPAN("N30");
+		overrideInupt.setSOSITENBFLIGHTSAVAIL("30");
+		overrideInupt.setSOSITEPOINTOFSALE("SEL");
+		overrideInupt.setSOSITEPOINTOFTICKETING("SEL");
+		overrideInupt.setSOSITEMINIMALTIME("N30");
+		overrideInupt.setSOSITEAPIV2SERVER("194.156.170.78");
+		overrideInupt.setSOSITEAPIV2SERVERUSERID("GUEST");
+		overrideInupt.setSOSITEAPIV2SERVERPWD("TAZ");
+
+		log.debug("embedded call");
+		EMBEDDEDTRANSACTION embedded = new EMBEDDEDTRANSACTION();
+		String schemaLocation = SchemaLocation.get(embeddedObject);
+		embedded.setSchemaRef(schemaLocation);
+		embedded.getContent().add(embeddedObject);
+		overrideInupt.setEMBEDDEDTRANSACTION(embedded);
+
+		overrideInupt.getAny().add(JAXBFactory.createElement("SOSITEMANUALETKTCMD", "TTP/ET"));
+		overrideInupt.getAny().add(JAXBFactory.createElement("SOSITESISERVERIP", "193.23.185.67"));
+		overrideInupt.getAny().add(JAXBFactory.createElement("SOSITESISERVERPORT", "18006"));
+		overrideInupt.getAny().add(JAXBFactory.createElement("SOSITESISAP", "1ASIXJCPU"));
+		overrideInupt.getAny().add(JAXBFactory.createElement("SOSITESIUSER", "UNSET"));
+		overrideInupt.getAny().add(JAXBFactory.createElement("SOSITESIPASSWORD", "UNSET"));
+		overrideInupt.getAny().add(JAXBFactory.createElement("SOSITESI1AXMLFROM", "SEPJCP"));
+		overrideInupt.getAny().add(JAXBFactory.createElement("SOSITECORPORATEID", "SEP-UAT"));
+		overrideInupt.getAny().add(JAXBFactory.createElement("SOSITEAPIV2SERVERPORT", "20002"));
+		overrideInupt.getAny().add(JAXBFactory.createElement("SOSITEPTCCONFVALIDATION", "FALSE"));
+		overrideInupt.getAny().add(JAXBFactory.createElement("SOSITESENDFOIDAIRLINE", "FALSE"));
+		overrideInupt.getAny().add(JAXBFactory.createElement("SOSITEMAXRESNUMATTEMPTS", "0"));
+
+		return overrideInupt;
+	}
+
+	public OverrideOutput keepAliveOverride() throws SOAPException {
+		OverrideInput overrideInupt = new OverrideInput();
+		overrideInupt.setTRANSACTIONID("Override");
+		overrideInupt.setSITE("CBFICBFI");
+		overrideInupt.setLANGUAGE("GB");
+		Object outObj = retailCon.sendAndReceive(overrideInupt, OverrideOutput.class);
+		setJsessionid(retailCon.getJsessionId());
+		OverrideOutput output = (OverrideOutput) outObj;
+		setReturnObject(output);
+		return output;
+	}
+
+	public static void main(String[] args) {
+		try {
+			OverrideAdapter adapter = new OverrideAdapter();
+			log.debug("{}", adapter.keepAliveOverride());
+		} catch (SOAPException e) {
+			e.printStackTrace();
+		}
+	}
+}
