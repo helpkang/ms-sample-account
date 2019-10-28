72e9c63a10b1e9e529203d28dfc651fb54f5245d
이병덕 <bbangduk@gmail.com>
2019-10-28 18:13:05 +0900

    aircalendarhelper

--- common-adapter/src/main/java/com/koreanair/common_adapter/dx/helper/AirCalendarHelper.java
+++ common-adapter/src/main/java/com/koreanair/common_adapter/dx/helper/AirCalendarHelper.java
@@ -15,6 +15,74 @@
  */
 package com.koreanair.common_adapter.dx.helper;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.koreanair.common.utils.DateUtil;
+import com.koreanair.common_adapter.dx.vo.AirCalendarOutputVO;
+import com.koreanair.common_adapter.dx.vo.AirMatrixCalendarVO;
+import com.koreanair.common_adapter.dx.vo.CalendarBoundVO;
+import com.koreanair.external.dx.vo.AirCalendar;
+import com.koreanair.external.dx.vo.AirCalendarReply;
+import com.koreanair.external.dx.vo.BoundCommon;
+import com.koreanair.external.dx.vo.FareFamilyWithServicesDictionaryItem;
+import com.koreanair.external.dx.vo.FareInfo;
+import com.koreanair.external.dx.vo.Price;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class AirCalendarHelper {
 
+	/**
+	 * <pre>
+	 * AirCalendarReply 를 AirCalendarOutput 로 변환 , 데이터 정규화
+	 * </pre>
+	 *
+	 * @param airCalendarReply
+	 */
+	public AirCalendarOutputVO convertAirCalendarReply2AirCalendarOutput(AirCalendarReply airCalendarReply) {
+		log.debug("convertAirCalendarReply2AirCalendarOutput start!");
+		AirCalendarOutputVO output = new AirCalendarOutputVO();
+
+		Map<String, String> cffMap = new HashMap<>();
+		Map<String, FareFamilyWithServicesDictionaryItem> ffMap = airCalendarReply.getDictionaries().getFareFamilyWithServices();
+		for (Map.Entry<String, FareFamilyWithServicesDictionaryItem> entry : ffMap.entrySet()) {
+			cffMap.put(entry.getKey(), entry.getValue().getCommercialFareFamily());
+		}
+
+
+		for (AirCalendar airCalendar : airCalendarReply.getData()) {
+			AirMatrixCalendarVO airMatrixCalendar = new AirMatrixCalendarVO();
+			airMatrixCalendar.setDepartureDate(DateUtil.getStringDate(airCalendar.getDepartureDate()));
+			airMatrixCalendar.setReturnDate(DateUtil.getStringDate(airCalendar.getReturnDate()));
+			airMatrixCalendar.setFareFamilyType(airCalendar.getFareFamilyCode());
+			airMatrixCalendar.setCommercialFareFamilyType(cffMap.get(airCalendar.getFareFamilyCode()));
+
+			for (FareInfo fareInfo : airCalendar.getFareInfos()) {
+				airMatrixCalendar.setFareClass(fareInfo.getFareClass());
+			}
+
+			int boundIdx = 1;
+			for(BoundCommon bound : airCalendar.getBounds()) {
+				CalendarBoundVO calendarBound = new CalendarBoundVO();
+				calendarBound.setBoundId(String.valueOf(boundIdx));
+				calendarBound.setDepartureAirport(bound.getOriginLocationCode());
+				calendarBound.setArrivalAirport(bound.getDestinationLocationCode());
+
+				for(Price price : bound.getPrices().getTotalPrices()) {
+					calendarBound.setBase(String.valueOf(price.getBase()));
+					calendarBound.setTotal(String.valueOf(price.getTotal()));
+					calendarBound.setTotalTax(String.valueOf(price.getTotalTaxes()));
+					output.setCurrency(price.getCurrencyCode());
+				}
+				airMatrixCalendar.getCalendarBoundList().add(calendarBound);
+				boundIdx++;
+			}
+
+			output.getAirMatrixCalendarList().add(airMatrixCalendar);
+		}
+
+		return output;
+	}
 }
