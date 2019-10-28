88cdcf7a4703b36eda9ef77f42feebcca2d2aae2
이병덕 <bbangduk@gmail.com>
2019-10-28 22:53:19 +0900

    airOffer helper

--- common-adapter/src/main/java/com/koreanair/common_adapter/dx/helper/AirOfferHelper.java
+++ common-adapter/src/main/java/com/koreanair/common_adapter/dx/helper/AirOfferHelper.java
@@ -15,6 +15,30 @@
  */
 package com.koreanair.common_adapter.dx.helper;
 
+import com.koreanair.external.dx.vo.AirOffer;
+import com.koreanair.external.dx.vo.AirOfferItem;
+import com.koreanair.external.dx.vo.AirOffersListReply;
+
 public class AirOfferHelper {
 
+	/**
+	 * <pre>
+	 * airOfferList에 중복된 값이나 사용하지 않는 값을 제거한다.
+	 * </pre>
+	 *
+	 * @param airOffers
+	 * @return
+	 */
+	public AirOffersListReply optimizeAirOfferReply(AirOffersListReply airOffers) {
+		for(AirOffer offer : airOffers.getData().getAirOffers()) {
+			for(AirOfferItem offerItem : offer.getOfferItems()) {
+				offerItem.setPrices(null);
+				offerItem.getAir().setFareInfos(null);
+			}
+		}
+		airOffers.getDictionaries().setLocation(null);
+		airOffers.getDictionaries().setCountry(null);
+		airOffers.getDictionaries().setAnonymousTraveler(null);
+		return airOffers;
+	}
 }
