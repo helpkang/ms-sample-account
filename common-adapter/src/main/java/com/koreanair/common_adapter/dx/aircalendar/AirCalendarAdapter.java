f3db10772e84c48f2e89d9f91abdaa0b281b988f
이병덕 <bbangduk@gmail.com>
2019-10-28 17:04:12 +0900

    aircalendarAdapater를 aircalendar package로 이동

--- common-adapter/src/main/java/com/koreanair/common_adapter/dx/aircalendar/AirCalendarAdapter.java
+++ common-adapter/src/main/java/com/koreanair/common_adapter/dx/aircalendar/AirCalendarAdapter.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.koreanair.common_adapter.dx.airopper;
+package com.koreanair.common_adapter.dx.aircalendar;
 
 import java.util.ArrayList;
 import java.util.Arrays;
